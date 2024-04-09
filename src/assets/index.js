49d5f305d11fc9dedd2db63c8e471791dd089009
xyzakash <akashdeep.singh@kandle.xyz>
2024-04-09 13:22:06 +0530

    added assets

---
+++ src/assets/index.js
@@ -0,0 +1,37 @@
+import appleLogo from './appleLogo.png';
+import email from './email.png';
+import happyBlob from './happyblob.png';
+import icon1 from './icon1.png';
+import icon2 from './icon2.png';
+import icon3 from './icon3.png';
+import icon4 from './icon4.png';
+import icon5 from './icon5.png';
+import icon6 from './icon6.png';
+import icon7 from './icon7.png';
+import icon8 from './icon8.png';
+import leftArrow from './left-arrow.png';
+import locationIcon from './location.png';
+import logo from './logo.png';
+import phone from './phone.png';
+import rightArrow from './right-arrow.png';
+import stars from './stars.png';
+
+export {
+  appleLogo,
+  email,
+  happyBlob,
+  icon1,
+  icon2,
+  icon3,
+  icon4,
+  icon5,
+  icon6,
+  icon7,
+  icon8,
+  leftArrow,
+  locationIcon,
+  logo,
+  phone,
+  rightArrow,
+  stars
+};
